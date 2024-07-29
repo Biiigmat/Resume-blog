3384b6bb0e98f5e0301f3d40ae52d9fa7e0349dc
Biiigmat <matinmoein511@gmail.com>
2024-07-29 22:56:24 +0330

    Final

--- project/blog/urls.py
+++ project/blog/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import index_dark_page
+from .views import index_dark_page , project_single_page , article_details_page ,index_page
+
+app_name='blog'
 
 urlpatterns = [
-    path('' , index_dark_page),
+    path('' , index_dark_page , name='index-dark'),
+    path('project-single/' , project_single_page , name='project-single' ),
+    path('article-details/', article_details_page , name='article-details-dark'),
+    path('index/' , index_page , name='index')
 ]
--- project/blog/views.py
+++ project/blog/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
 def index_dark_page (request):
-    return render (request , 'index_dark.html')
+    return render (request , 'blog/index_dark.html')
+
+def project_single_page (request):
+    return render (request , 'blog/project-single_dark.html')
+
+def article_details_page (request):
+    return render (request , 'blog/article-details_dark.html')
+
+def index_page (request):
+    return render (request , 'blog/index.html')
